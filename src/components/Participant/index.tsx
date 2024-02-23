5566fa0e79450dc8ec0835cbbabfb312428a97bd
Dérick <6derick6@gmail.com>
2024-02-22 21:35:02 -0300

    Criando Componente Participante

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { styles } from "./style";
+
+export function Participant(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.name}>Dérick Trennepohl</Text>
+      <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from './styles';
+
+import { Participant } from "../../components/Participant";
+
+export function Home(){
+  function handleParticipantAdd(){
+
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do participante'
+          placeholderTextColor='#6B6B6B'
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+    </View>
+  )
+}
