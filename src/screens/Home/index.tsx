289cf3d35602e8205605724ee7c8b3b20209ae27
Dérick <6derick6@gmail.com>
2024-02-28 21:21:05 -0300

    Entendendo o Render

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,20 @@
+import React, { useState } from "react";
 import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
 import { styles } from './styles';
 
 import { Participant } from "../../components/Participant";
 
 export function Home(){
-  const participants = ['Dérick', 'Braian', 'Augusto', 'Gordo', 'Leticia', 'Mello', 'Guilherme', 'Alan', 'Wesley'];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setparticipantName] = useState('');
 
   function handleParticipantAdd(){
-    if(participants.includes("Dérick")){
+    if(participants.includes(participantName)){
       return Alert.alert("Participante existe", "Já existe o participante na lista com esse nome.");
     }
+
+    setParticipants(prevState => [...prevState, participantName]);
+    setparticipantName('');
   }
 
   function handleparticipantRemove(name: string){
@@ -35,7 +40,10 @@ export function Home(){
           style={styles.input} 
           placeholder='Nome do participante'
           placeholderTextColor='#6B6B6B'
+          onChangeText={setparticipantName}
+          value={participantName}
         />
+
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
